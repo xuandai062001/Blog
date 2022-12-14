c91b3bce62553b0af6d0eb71537e68feffdb237a
unknown <xuandai062001@gmail.com>
2022-12-14 13:27:52 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AllPosts from "./components/AllPosts.js";
+import OnePost from "./components/OnePost.js";
+import Header from "./components/Header.js";
+import FilteredPosts from "./views/FilteredPosts.js"
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+      <Header />
+      <div>
+      <Routes>
+	 	   <Route element={<AllPosts />} path="/" />
+       
+       <Route element={<OnePost />} path="/:slug" />
+
+       <Route element={<FilteredPosts />} path="/FilteredPosts/:title" />
+      </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect }from "react";
+import sanityClient from "../Client";
+import { NavLink, Link } from "react-router-dom";
+
+const NavBar = () => {
+    const [categories, setCategories] = useState(null);
+
+	useEffect(() => {
+		sanityClient
+			.fetch(
+				`*[_type == "category"]{
+      title,
+      slug,
+    }`
+			)
+			.then((data) => setCategories(data))
+			.catch(console.error);
+	}, []);
+
+	return (
+		<nav className="container lg:px-0 px-5 py-2 lg:py-0 lg:w-3/4 w-full mx-auto flex flex-col lg:flex-row justify-between h-20 items-center font-bold">
+			<NavLink to="/">
+				<p className="text-xl lg:text-2xl">😋DAVID CODE</p>
+			</NavLink>
+			
+			<ul className="flex navbar">
+				<li>
+					<NavLink
+						to="/"
+						exact
+						className="nav-link"
+						activeClassName="active-link"
+					>
+						Home
+					</NavLink>
+				</li>
+				<li className="categories pl-4">
+					<span>Categories</span>
+					<ul>
+						{categories &&
+						categories.map((category) => (
+							<li>
+								<Link to={"/filteredposts/" + category.title}>
+									<button
+									>
+										{category.title}
+									</button>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</li>
+				<a className="pl-4" href="https://profile.xuandai.live/" target="_blank">About Me</a>
+			</ul>
+			
+		</nav>
+	);
+};
+
+export default  NavBar;
