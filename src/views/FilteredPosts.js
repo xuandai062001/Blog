c83763799ceb9f153e107b648bf0c3a09e741b5c
unknown <xuandai062001@gmail.com>
2022-12-19 14:41:00 +0700

    2: 40 pm 19/12

--- src/views/FilteredPosts.js
+++ src/views/FilteredPosts.js
@@ -1,10 +1,11 @@
 // src/components/AllPosts.js
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams} from "react-router-dom";
 import sanityClient from "../Client.js";
+import PaginatedItems from "../components/Pagination/PaginatedItems.js";
 
 export default function FilterPosts() {
-  const [allPostsData, setAllPosts] = useState(null);
+  const [allPostsData, setAllPosts] = useState([]);
   const { title } = useParams();
 
   useEffect(() => {
@@ -21,49 +22,17 @@ export default function FilterPosts() {
       }
     }`
       )
-      .then((data) => setAllPosts(data))
+      .then(res => {
+        setAllPosts(res)
+      })
       .catch(console.error);
   }, [allPostsData, title]);
 
-  if (!allPostsData) return <div>Loading...</div>;
+  if (allPostsData.length === 0 ) return <div>Loading...</div>;
 
   return (
-    <div className="bg-green-100 min-h-screen sm: p-4 md:p-12">
-      <div className="container mx-auto">
-        <h2 className="text-5xl flex justify-center cursive">Blog Posts</h2>
-        <h3 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome to my blog posts page!
-        </h3>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {allPostsData &&
-            allPostsData.map((post, index) => (
-              <Link to={"/" + post.slug.current} key={post.slug.current}>
-                <span
-                  className="block h-64 relative rounded shadow leading-snug bg-white
-                      border-l-8 border-green-400"
-                  key={index}
-                >
-                  <img
-                    className="w-full h-full rounded-r object-cover absolute"
-                    src={post.mainImage.asset.url}
-                    alt=""
-                  />
-                  <span
-                    className="block relative h-full flex justify-end items-end pr
-                      -4 pb-4"
-                  >
-                    <h2
-                      className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700
-                        text-red-100 bg-opacity-75 rounded w-full"
-                    >
-                      {post.title}
-                    </h2>
-                  </span>
-                </span>
-              </Link>
-            ))}
-        </div>
-      </div>
-    </div>
+    <> 
+    <PaginatedItems itemsPerPage={12} allPostsData={allPostsData}/>
+    </>
   );
 }
